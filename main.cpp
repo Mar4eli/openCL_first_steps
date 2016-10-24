9f72907e25dbb01ee707dba768d83b36d11e3b28
Pavel Antonov <p.antonov@list.ru>
2016-10-25 02:33:01 +0300

    Работает на double варианты ускорения: 1. ulong 2. внутрь вставить циклы.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,69 @@
 #include <iostream>
 #include <CL/cl.hpp>
 #include <QDebug>
+#include <QTime>
 
-#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+const int ITERATION_STEP=1000;
 
+void func(ulong z, double m_inNumber, double n_max, cl::Context context, cl::Device default_device, cl::Program program)
+{
+    double offset[ITERATION_STEP];
+    double inNumber[ITERATION_STEP];
+    double max[ITERATION_STEP];
 
-const int ITERATION_STEP=1;
+//    std::cout<<"before FOR \n";
+    for(ulong i=0; i < ITERATION_STEP; i++)
+    {
+        offset[i] = i+z;
+        inNumber[i] = m_inNumber;
+        max[i] = n_max;
+    }
+
+//    std::cout<<"for is done \n";
+
+    cl::Buffer buffer_inNumber(context,CL_MEM_READ_WRITE,sizeof(double)*ITERATION_STEP);
+    cl::Buffer buffer_offset(context,CL_MEM_READ_WRITE,sizeof(double)*ITERATION_STEP);
+    cl::Buffer buffer_max(context,CL_MEM_READ_WRITE,sizeof(double)*ITERATION_STEP);
+    cl::Buffer buffer_rez(context,CL_MEM_READ_WRITE,sizeof(double)*ITERATION_STEP);
+    cl::Buffer buffer_rez2(context,CL_MEM_READ_WRITE,sizeof(double)*ITERATION_STEP);
+
+//    std::cout<<"buffers created \n";
+    //create queue to which we will push commands for the device.
+    cl::CommandQueue queue(context,default_device);
+
+    //write arrays A and B to the device
+    queue.enqueueWriteBuffer(buffer_inNumber,CL_TRUE,0,sizeof(double)*ITERATION_STEP,inNumber);
+    queue.enqueueWriteBuffer(buffer_offset,CL_TRUE,0,sizeof(double)*ITERATION_STEP,offset);
+    queue.enqueueWriteBuffer(buffer_max,CL_TRUE,0,sizeof(double)*ITERATION_STEP,max);
+//    std::cout<<"buffers in queue \n";
+
+    //run the kernel
+    cl::Kernel kernel_add=cl::Kernel(program,"simple_add");
+    kernel_add.setArg(0,buffer_offset);
+    kernel_add.setArg(1,buffer_inNumber);
+    kernel_add.setArg(2,buffer_max);
+    kernel_add.setArg(3,buffer_rez);
+    kernel_add.setArg(4,buffer_rez2);
+//    std::cout<<"buffers in kernel \n";
+
+    //посмотреть, что делает.
+    queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,cl::NDRange(ITERATION_STEP),cl::NullRange);
+    queue.finish();
+//    std::cout<<"kernels added \n";
+
+    double rez1[ITERATION_STEP];
+    double rez2[ITERATION_STEP];
+    queue.enqueueReadBuffer(buffer_rez,CL_TRUE,0,sizeof(double)*ITERATION_STEP,rez1);
+    queue.enqueueReadBuffer(buffer_rez2,CL_TRUE,0,sizeof(double)*ITERATION_STEP,rez2);
+    for(ulong i=0; i < ITERATION_STEP; i++)
+    {
+        if(rez1[i] !=0 || rez2[i] != 0)
+        {
+            std::cout<<rez1[i]<<" "<<rez2[i]<<"\n";
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
 //    QApplication a(argc, argv);
@@ -50,34 +108,25 @@ int main(int argc, char *argv[])
             //примечание не удалось забиндить целочисленное значение в sqrt
             std::string kernel_code=
                     "#pragma OPENCL EXTENSION cl_khr_fp64 : enable \n"
-                    "void kernel simple_add(global double *offset, global double *limit, global double *inNumber, global double *rez){ \
-                        double max = offset[get_global_id(0)] + limit[get_global_id(0)]; \
-                        if(offset[get_global_id(0)] == 0){ \
-                            offset[get_global_id(0)]=offset[get_global_id(0)]+1;} \
-                        double squareNum =(offset[get_global_id(0)]-1)*(offset[get_global_id(0)]-1); \
-                        ulong i; \
-                        float j,rootNum; \
-                        float z; \
-                        double sequence = 1 + (offset[get_global_id(0)]-1)*2; \
-                        for(i=offset[get_global_id(0)]; i < max; ++i){ \
-                            squareNum=squareNum+sequence; \
-                            sequence=sequence+2; \
+                    "void kernel simple_add(global double *offset, global double *inNumber, global double *max, global double *rez, global double *rez2){ \
+                        if(max[get_global_id(0)] > offset[get_global_id(0)]){\
+                            double squareNum =(offset[get_global_id(0)])*(offset[get_global_id(0)]); \
+                            double j,rootNum; \
+                            double z; \
                             if(squareNum <= inNumber[get_global_id(0)]){ \
                                 j = inNumber[get_global_id(0)] - squareNum; \
                                 if(j != 0.0){ \
                                     rootNum = sqrt(j); \
                                     if(modf(rootNum,&z) == 0){ \
-                                        rez[get_global_id(0)*2] = rootNum; \
-                                        rez[get_global_id(0)*2+1] = i; \
+                                        rez[get_global_id(0)] = rootNum; \
+                                        rez2[get_global_id(0)] = offset[get_global_id(0)]; \
                                     } \
                                 }else{ \
-                                    rez[get_global_id(0)*2]=j; \
-                                    rez[get_global_id(0)*2+1]=i; \
+                                    rez[get_global_id(0)]=j; \
+                                    rez2[get_global_id(0)]=offset[get_global_id(0)]; \
                                 } \
-                            }else{ \
-                                break; \
                             } \
-                        } \
+                        }\
                     }";
 
             std::cout<<"kernel added \n";
@@ -102,68 +151,27 @@ int main(int argc, char *argv[])
             //В таком случае, нам не надо исследовать числа большие, чем sqrt(заданное_число) c округлением в меньшую сторону.
 
             //Подготовка данных
-            double m_inNumber = 2547483647;
-            double max_right_value = sqrt(m_inNumber);
-            double iterations = max_right_value/ITERATION_STEP;
-            ulong sizeIter = iterations+1;
-
-            std::cout<<"iterations found="<<iterations<<" \n";
-
-            double offset[sizeIter];
-            double inNumber[sizeIter];
-            double iterationsLimit[sizeIter];
-
-            std::cout<<"before FOR \n";
-            for(ulong i=0; i <= iterations; i++)
+            double m_inNumber = 6400000000000000;
+            const double max_right_value = sqrt(m_inNumber);
+            //std::cout<<max_right_value;
+            ulong sizeIter;
+            if(max_right_value > ITERATION_STEP)
             {
-                offset[i] = i*ITERATION_STEP;
-                inNumber[i] = m_inNumber;
-                iterationsLimit[i] = ITERATION_STEP;
+                sizeIter = ITERATION_STEP;
             }
-
-            std::cout<<"for is done \n";
-
-            cl::Buffer buffer_inNumber(context,CL_MEM_READ_WRITE,sizeof(double)*sizeIter);
-            cl::Buffer buffer_offset(context,CL_MEM_READ_WRITE,sizeof(double)*sizeIter);
-            cl::Buffer buffer_limit(context,CL_MEM_READ_WRITE,sizeof(double)*sizeIter);
-            cl::Buffer buffer_rez(context,CL_MEM_READ_WRITE,sizeof(double)*sizeIter*2);
-
-            std::cout<<"buffers created \n";
-            //create queue to which we will push commands for the device.
-            cl::CommandQueue queue(context,default_device);
-
-            //write arrays A and B to the device
-            queue.enqueueWriteBuffer(buffer_inNumber,CL_TRUE,0,sizeof(double)*sizeIter,inNumber);
-            queue.enqueueWriteBuffer(buffer_offset,CL_TRUE,0,sizeof(double)*sizeIter,offset);
-            queue.enqueueWriteBuffer(buffer_limit,CL_TRUE,0,sizeof(double)*sizeIter,iterationsLimit);
-            std::cout<<"buffers in queue \n";
-
-            //run the kernel
-            cl::Kernel kernel_add=cl::Kernel(program,"simple_add");
-            kernel_add.setArg(0,buffer_offset);
-            kernel_add.setArg(1,buffer_limit);
-            kernel_add.setArg(2,buffer_inNumber);
-            kernel_add.setArg(3,buffer_rez);
-            std::cout<<"buffers in kernel \n";
-
-            //посмотреть, что делает.
-            queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,cl::NDRange(sizeIter),cl::NullRange);
-            queue.finish();
-            std::cout<<"kernels added \n";
-
-            double rez[sizeIter*2];
-            queue.enqueueReadBuffer(buffer_rez,CL_TRUE,0,sizeof(double)*sizeIter*2,rez);
-            for(ulong i=0; i < sizeIter*2; i+=2)
+            else
             {
-                if(rez[i] !=0 || rez[i+1] != 0)
-                {
-                    std::cout<<rez[i]<<" "<<rez[i+1]<<"\n";
-                }
+                sizeIter = max_right_value;
             }
-
-
+            QTime start = QTime::currentTime();
+            for(ulong z=0; z <= max_right_value; z=z+ITERATION_STEP)
+            {
+                func(z,m_inNumber,max_right_value,context,default_device,program);
+            }
+            std::cout<<"time="<<start.elapsed()<<"\n";
             return 0;
 
 
     //return a.exec();
 }
+
