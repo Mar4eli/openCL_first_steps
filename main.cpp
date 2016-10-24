b1e4cc1b8c38d96b4e855ffdc6a6530b513b1567
Pavel Antonov <p.antonov@list.ru>
2016-10-25 01:17:07 +0300

    Работает double precision, но на числах более 2547483647 падаем.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <CL/cl.hpp>
 #include <QDebug>
 
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+
+
 const int ITERATION_STEP=1;
 int main(int argc, char *argv[])
 {
@@ -24,7 +27,7 @@ int main(int argc, char *argv[])
         }
 
         cl::Platform default_platform=all_platforms[0];
-        std::cout<< "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<"\n";
+        std::cout<< "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<" v"<<default_platform.getInfo<CL_PLATFORM_VERSION>()<<"\n";
 
 
 
@@ -36,7 +39,8 @@ int main(int argc, char *argv[])
                 exit(1);
             }
             cl::Device default_device=all_devices[0];
-            std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<<"\n";
+            std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<<" "<<default_device.getInfo<CL_DEVICE_PREFERRED_VECTOR_WIDTH_DOUBLE>()<<"\n";
+            std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_EXTENSIONS>()<<"\n";
 
 
             cl::Context context({default_device});
@@ -45,35 +49,36 @@ int main(int argc, char *argv[])
             std::cout<<"kernel binding \n";
             //примечание не удалось забиндить целочисленное значение в sqrt
             std::string kernel_code=
-                    "void kernel simple_add(global int *offset, global int *limit, global int *inNumber, global int *rez){"
-                        "int max = offset[get_global_id(0)] + limit[get_global_id(0)];"
-                        "if(offset[get_global_id(0)] == 0){"
-                            "offset[get_global_id(0)]=offset[get_global_id(0)]+1;}"
-                        "int squareNum =(offset[get_global_id(0)]-1)*(offset[get_global_id(0)]-1);"
-                        "int i;"
-                        "float j,rootNum;"
-                        "float z;"
-                        "int sequence = 1 + (offset[get_global_id(0)]-1)*2;"
-                        "for(i=offset[get_global_id(0)]; i < max; ++i){"
-                            "squareNum=squareNum+sequence;"
-                            "sequence=sequence+2;"
-                            "if(squareNum <= inNumber[get_global_id(0)]){"
-                                "j = inNumber[get_global_id(0)] - squareNum;"
-                                "if(j != 0.0){"
-                                    "rootNum = sqrt(j);"
-                                    "if(modf(rootNum,&z) == 0){"
-                                        "rez[get_global_id(0)*2] = rootNum;"
-                                        "rez[get_global_id(0)*2+1] = i;"
-                                    "}"
-                                "}else{"
-                                    "rez[get_global_id(0)*2]=j;"
-                                    "rez[get_global_id(0)*2+1]=i;"
-                                "}"
-                            "}else{"
-                                "break;"
-                            "}"
-                        "}"
-                    "}";
+                    "#pragma OPENCL EXTENSION cl_khr_fp64 : enable \n"
+                    "void kernel simple_add(global double *offset, global double *limit, global double *inNumber, global double *rez){ \
+                        double max = offset[get_global_id(0)] + limit[get_global_id(0)]; \
+                        if(offset[get_global_id(0)] == 0){ \
+                            offset[get_global_id(0)]=offset[get_global_id(0)]+1;} \
+                        double squareNum =(offset[get_global_id(0)]-1)*(offset[get_global_id(0)]-1); \
+                        ulong i; \
+                        float j,rootNum; \
+                        float z; \
+                        double sequence = 1 + (offset[get_global_id(0)]-1)*2; \
+                        for(i=offset[get_global_id(0)]; i < max; ++i){ \
+                            squareNum=squareNum+sequence; \
+                            sequence=sequence+2; \
+                            if(squareNum <= inNumber[get_global_id(0)]){ \
+                                j = inNumber[get_global_id(0)] - squareNum; \
+                                if(j != 0.0){ \
+                                    rootNum = sqrt(j); \
+                                    if(modf(rootNum,&z) == 0){ \
+                                        rez[get_global_id(0)*2] = rootNum; \
+                                        rez[get_global_id(0)*2+1] = i; \
+                                    } \
+                                }else{ \
+                                    rez[get_global_id(0)*2]=j; \
+                                    rez[get_global_id(0)*2+1]=i; \
+                                } \
+                            }else{ \
+                                break; \
+                            } \
+                        } \
+                    }";
 
             std::cout<<"kernel added \n";
             sources.push_back({kernel_code.c_str(),kernel_code.length()});
@@ -97,57 +102,43 @@ int main(int argc, char *argv[])
             //В таком случае, нам не надо исследовать числа большие, чем sqrt(заданное_число) c округлением в меньшую сторону.
 
             //Подготовка данных
-            int m_inNumber = 400;
-            int max_right_value = sqrt(m_inNumber);
-            int iterations = max_right_value/ITERATION_STEP;
-            int sizeIter = iterations+1;
+            double m_inNumber = 2547483647;
+            double max_right_value = sqrt(m_inNumber);
+            double iterations = max_right_value/ITERATION_STEP;
+            ulong sizeIter = iterations+1;
 
             std::cout<<"iterations found="<<iterations<<" \n";
 
-            int offset[sizeIter];
-            int inNumber[sizeIter];
-            int iterationsLimit[sizeIter];
+            double offset[sizeIter];
+            double inNumber[sizeIter];
+            double iterationsLimit[sizeIter];
 
             std::cout<<"before FOR \n";
-            for(int i=0; i <= iterations; i++)
+            for(ulong i=0; i <= iterations; i++)
             {
                 offset[i] = i*ITERATION_STEP;
-                std::cout<<offset[i]<<" "<<i<<"\n";
                 inNumber[i] = m_inNumber;
                 iterationsLimit[i] = ITERATION_STEP;
             }
 
             std::cout<<"for is done \n";
-//            offset[sizeIter] = (iterations+1)*ITERATION_STEP;
-//            inNumber[sizeIter] = m_inNumber;
-//            iterationsLimit[sizeIter] = ITERATION_STEP;
 
-            std::cout<<"array filled \n";
-
-            // create buffers on the device
-//            cl::Buffer buffer_A(context,CL_MEM_READ_WRITE,sizeof(int)*10);
-//            cl::Buffer buffer_B(context,CL_MEM_READ_WRITE,sizeof(int)*10);
-//            cl::Buffer buffer_C(context,CL_MEM_READ_WRITE,sizeof(int)*10);
-
-//            int A[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-//            int B[] = {0, 1, 2, 0, 1, 2, 0, 1, 2, 0};
-
-            cl::Buffer buffer_inNumber(context,CL_MEM_READ_WRITE,sizeof(int)*sizeIter);
-            cl::Buffer buffer_offset(context,CL_MEM_READ_WRITE,sizeof(int)*sizeIter);
-            cl::Buffer buffer_limit(context,CL_MEM_READ_WRITE,sizeof(int)*sizeIter);
-            cl::Buffer buffer_rez(context,CL_MEM_READ_WRITE,sizeof(int)*sizeIter*2);
+            cl::Buffer buffer_inNumber(context,CL_MEM_READ_WRITE,sizeof(double)*sizeIter);
+            cl::Buffer buffer_offset(context,CL_MEM_READ_WRITE,sizeof(double)*sizeIter);
+            cl::Buffer buffer_limit(context,CL_MEM_READ_WRITE,sizeof(double)*sizeIter);
+            cl::Buffer buffer_rez(context,CL_MEM_READ_WRITE,sizeof(double)*sizeIter*2);
 
             std::cout<<"buffers created \n";
-//            //create queue to which we will push commands for the device.
+            //create queue to which we will push commands for the device.
             cl::CommandQueue queue(context,default_device);
 
-//            //write arrays A and B to the device
-            queue.enqueueWriteBuffer(buffer_inNumber,CL_TRUE,0,sizeof(int)*sizeIter,inNumber);
-            queue.enqueueWriteBuffer(buffer_offset,CL_TRUE,0,sizeof(int)*sizeIter,offset);
-            queue.enqueueWriteBuffer(buffer_limit,CL_TRUE,0,sizeof(int)*sizeIter,iterationsLimit);
+            //write arrays A and B to the device
+            queue.enqueueWriteBuffer(buffer_inNumber,CL_TRUE,0,sizeof(double)*sizeIter,inNumber);
+            queue.enqueueWriteBuffer(buffer_offset,CL_TRUE,0,sizeof(double)*sizeIter,offset);
+            queue.enqueueWriteBuffer(buffer_limit,CL_TRUE,0,sizeof(double)*sizeIter,iterationsLimit);
             std::cout<<"buffers in queue \n";
 
-//            //run the kernel
+            //run the kernel
             cl::Kernel kernel_add=cl::Kernel(program,"simple_add");
             kernel_add.setArg(0,buffer_offset);
             kernel_add.setArg(1,buffer_limit);
@@ -160,13 +151,9 @@ int main(int argc, char *argv[])
             queue.finish();
             std::cout<<"kernels added \n";
 
-//            //int C[10];
-//            //read result C from the device to array C
-//            //queue.enqueueReadBuffer(buffer_C,CL_TRUE,0,sizeof(int)*10,C);
-
-            int rez[sizeIter*2];
-            queue.enqueueReadBuffer(buffer_rez,CL_TRUE,0,sizeof(int)*sizeIter*2,rez);
-            for(int i=0; i < sizeIter*2; i+=2)
+            double rez[sizeIter*2];
+            queue.enqueueReadBuffer(buffer_rez,CL_TRUE,0,sizeof(double)*sizeIter*2,rez);
+            for(ulong i=0; i < sizeIter*2; i+=2)
             {
                 if(rez[i] !=0 || rez[i+1] != 0)
                 {
