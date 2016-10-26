4f491851eb47f757891559d6860ec213cc21cb30
Pavel Antonov <p.antonov@list.ru>
2016-10-26 23:45:26 +0300

    6,188 Перенёс зануление rez и rez2 на GPU

--- openCLWindow.cpp
+++ openCLWindow.cpp
@@ -182,16 +182,23 @@ void openCLWindow::on_pushButton_2_clicked()
 
     std::string kernel_code=
             "#pragma OPENCL EXTENSION cl_khr_fp64 : enable \n"
-            "void kernel simple_add(global double *offset, global double *inNumber, global double *max, global ulong *rez, global ulong *rez2){ \n \
+            "void kernel simple_add(global double *offset, global double *inNumber, global double *max, global ulong *rez, global ulong *rez2, global int *zindK){ \n \
                 ulong limit = 1000; \n \
-                double curOffset = offset[get_global_id(0)]; \n \
-                double curInNumber = inNumber[get_global_id(0)]; \n \
+                ulong curGlobalId = get_global_id(0); \n \
+                int curZindK = zindK[curGlobalId]; \n \
+                for(ulong j=0; j < limit; j++) \n \
+                {\n \
+                    rez[curZindK*limit+j] = 0; \n \
+                    rez2[curZindK*limit+j] = 0; \n \
+                }\n \
+                double curOffset = offset[curGlobalId]; \n \
+                double curInNumber = inNumber[curGlobalId]; \n \
                 double previous1 = 0; \
                 ulong previous2 = 0; \
                 ulong maxIt = curOffset + limit; \n \
-                if(maxIt > max[get_global_id(0)]) \n \
+                if(maxIt > max[curGlobalId]) \n \
                 { \n \
-                    maxIt = max[get_global_id(0)]; \n \
+                    maxIt = max[curGlobalId]; \n \
                 } \n \
                 ulong index = 0; \n \
                 double squareNum = curOffset*curOffset;; \n \
@@ -208,15 +215,15 @@ void openCLWindow::on_pushButton_2_clicked()
                             if(modf(rootNum,&z) == 0.0){ \n \
                                 if(!(previous1 == (ulong)rootNum && previous2 == i))\n \
                                 {\n \
-                                    rez2[get_global_id(0)*limit+index] = rootNum; \n \
-                                    rez[get_global_id(0)*limit+index] = i; \n \
+                                    rez2[curGlobalId*limit+index] = rootNum; \n \
+                                    rez[curGlobalId*limit+index] = i; \n \
                                     previous1 = rootNum; \n \
                                     previous2 = i; \n \
                                 }\n \
                             } \n \
                         }else{ \n \
-                            rez2[get_global_id(0)*limit+index]=j; \n \
-                            rez[get_global_id(0)*limit+index]=i; \n \
+                            rez2[curGlobalId*limit+index]=j; \n \
+                            rez[curGlobalId*limit+index]=i; \n \
                         } \n \
                     } \n \
                     squareNum = squareNum + sequence; \n \
@@ -261,24 +268,20 @@ void openCLWindow::on_pushButton_2_clicked()
     double max = max_right_value;
     ulong rez1[ITERATION_STEP*ITERATION_SIZE];
     ulong rez2[ITERATION_STEP*ITERATION_SIZE];
+    int zindK[ITERATION_SIZE];
 
     cl::Buffer buffer_inNumber(context,CL_MEM_READ_WRITE|CL_MEM_ALLOC_HOST_PTR,sizeof(double),&inNumber);
     cl::Buffer buffer_offset(context,CL_MEM_READ_WRITE|CL_MEM_ALLOC_HOST_PTR,sizeof(double)*ITERATION_SIZE,offset);
     cl::Buffer buffer_max(context,CL_MEM_READ_WRITE|CL_MEM_ALLOC_HOST_PTR,sizeof(double),&max);
     cl::Buffer buffer_rez(context,CL_MEM_READ_WRITE|CL_MEM_ALLOC_HOST_PTR,sizeof(ulong)*ITERATION_SIZE*ITERATION_STEP,rez1);
     cl::Buffer buffer_rez2(context,CL_MEM_READ_WRITE|CL_MEM_ALLOC_HOST_PTR,sizeof(ulong)*ITERATION_SIZE*ITERATION_STEP,rez2);
-
+    cl::Buffer buffer_zindK(context,CL_MEM_READ_WRITE|CL_MEM_ALLOC_HOST_PTR,sizeof(ulong)*ITERATION_SIZE,zindK);
     int xz = 0;
     double last=0;
     while(last < max_right_value)
     {
         for(zind=0; zind < ITERATION_SIZE; zind++)
         {
-            for(ulong j=0; j < ITERATION_STEP; j++)
-            {
-                rez1[zind*ITERATION_STEP+j] = 0;
-                rez2[zind*ITERATION_STEP+j] = 0;
-            }
             double tmp = zind*ITERATION_STEP+xz*ITERATION_SIZE*ITERATION_STEP;
             if(tmp <= max_right_value)
             {
@@ -297,6 +300,7 @@ void openCLWindow::on_pushButton_2_clicked()
         //write arrays A and B to the device
         queue.enqueueWriteBuffer(buffer_inNumber,CL_FALSE,0,sizeof(double),&inNumber);
         queue.enqueueWriteBuffer(buffer_offset,CL_FALSE,0,sizeof(double)*ITERATION_SIZE,offset);
+        queue.enqueueWriteBuffer(buffer_zindK,CL_FALSE,0,sizeof(double)*ITERATION_SIZE,zindK);
         queue.enqueueWriteBuffer(buffer_max,CL_FALSE,0,sizeof(double),&max);
     //    std::cout<<"buffers in queue \n";
 
@@ -307,9 +311,10 @@ void openCLWindow::on_pushButton_2_clicked()
         kernel_add.setArg(2,buffer_max);
         kernel_add.setArg(3,buffer_rez);
         kernel_add.setArg(4,buffer_rez2);
+        kernel_add.setArg(5,buffer_zindK);
 
         //посмотреть, что делает.
-        queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS,cl::NullRange);
+        queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,cl::NDRange(100),cl::NullRange);
         queue.finish();
 
         queue.enqueueReadBuffer(buffer_rez,CL_FALSE,0,sizeof(ulong)*ITERATION_SIZE*ITERATION_STEP,rez1);
